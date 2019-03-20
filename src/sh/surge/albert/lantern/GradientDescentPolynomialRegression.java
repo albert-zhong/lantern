287ab2d2a3c3e89545f3bdf418386ae434abfd69
Albert Zhong <albertzhong@hotmail.com>
2019-03-19 22:07:23 -0700

    Add general algorithm for all polynomial regression problems

---
+++ src/sh/surge/albert/lantern/GradientDescentPolynomialRegression.java
@@ -0,0 +1,121 @@
+package sh.surge.albert.lantern;
+
+import java.awt.*;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class GradientDescentPolynomialRegression {
+
+    public GradientDescentPolynomialRegression(int degree){
+        numberOfParameters = degree + 1;
+        parameters = new double[numberOfParameters];
+
+        for (int i=0; i<numberOfParameters; i++){
+            parameters[i] = 0;
+        }
+    }
+
+    private int numberOfParameters;
+    private double[] parameters;
+    private ArrayList<Point> points = new ArrayList<>();
+
+    private final double learningRate = 0.002;
+    private final double precision = 0.000001;
+
+    public void addPoints(ArrayList<Point> newPoints){
+        points.addAll(newPoints);
+    }
+
+    public void train() {
+        for (int i=0; true; i++) {
+
+            double partialDerivatives[] = getPartialDerivatives();
+
+            if (isConverged(partialDerivatives)) {
+                finish();
+                System.out.println("Took " + i + " tries!");
+                break;
+            }
+
+            double[] tempParameters = parameters.clone();
+
+            for (int j=0; j<tempParameters.length; j++) {
+                tempParameters[j] = parameters[j] - learningRate*partialDerivatives[j];
+            }
+
+            parameters = tempParameters;
+        }
+    }
+
+    public double[] getPartialDerivatives() {
+        double[] sums = new double[numberOfParameters];
+        for (int i=0; i<sums.length; i++) { // Sets all original values of sums to 0
+            sums[i] = 0;
+        }
+
+        for (int i=0; i<points.size(); i++) { // Loops through all points
+            double currentX = points.get(i).getX();
+            double currentY = points.get(i).getY();
+
+            double delta = hypothesis(currentX) - currentY;
+
+            for (int j=0; j<sums.length; j++) {  // Calculate sums for each partial derivative, from j=0
+                sums[j] += (delta * Math.pow(currentX, j));
+            }
+        }
+
+        for (int i=0; i<sums.length; i++) { // Multiplies each partial derivative by 1/m
+            sums[i] /= points.size();
+        }
+
+        return sums;
+    }
+
+    private double hypothesis(double x) {  // Returns hypothesis prediction
+        double hypothesis = 0;
+        for (int i=0; i<numberOfParameters; i++) {
+            hypothesis += parameters[i]*Math.pow(x, i);
+        }
+        return hypothesis;
+    }
+
+    private void show() {
+        for (int i=0; i<parameters.length; i++) {
+            System.out.print(i + ": " + parameters[i] + ", ");
+        }
+    }
+
+    private boolean isConverged(double[] numbers) {
+        for (int i=0; i<numbers.length; i++) {
+            if (Math.abs(numbers[i]) > precision) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void finish(){ // Rounds parameters and prints results
+        for (int i=0; i<parameters.length; i++){
+            parameters[i] = round(parameters[i], 4);
+        }
+        show();
+    }
+
+
+    private double round(double number, int decimalPlace) {
+        if (decimalPlace < 1) {
+            throw new IllegalArgumentException("Must round to at least one decimal place!");
+        }
+
+        StringBuilder pattern = new StringBuilder("#.");
+        for (int i=0; i<decimalPlace; i++) {
+            pattern.append("#");
+        }
+        DecimalFormat df = new DecimalFormat(pattern.toString());
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        return Double.valueOf(df.format(number));
+    }
+
+}
--- src/sh/surge/albert/lantern/Main.java
+++ src/sh/surge/albert/lantern/Main.java
@@ -6,32 +6,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main (String args[]) {
 
-        Point p1 = new Point(8, 3);
-        Point p2 = new Point(2, 10);
-        Point p3 = new Point(11, 3);
-        Point p4 = new Point(6, 6);
-        Point p5 = new Point(5, 8);
-        Point p6 = new Point(4, 12);
-        Point p7 = new Point(12, 1);
-        Point p8 = new Point(9, 4);
-        Point p9= new Point(6, 9);
-        Point p10 = new Point(1, 14);
+        Point q1 = new Point(0,5);
+        Point q2 = new Point(1,10);
+        Point q3 = new Point(3,38);
+        Point q4 = new Point(4,17);
+        Point q5 = new Point(8,65);
+        ArrayList<Point> curve = new ArrayList<>();
+        curve.add(q1);
+        curve.add(q2);
+        curve.add(q3);
 
-        ArrayList<Point> points = new ArrayList<>();
-        points.add(p1);
-        points.add(p2);
-        points.add(p3);
-        points.add(p4);
-        points.add(p5);
-        points.add(p6);
-        points.add(p7);
-        points.add(p8);
-        points.add(p9);
-        points.add(p10);
-
-        GradientDescentLinearRegression line = new GradientDescentLinearRegression();
-        line.addPoints(points);
-        line.train();
+        GradientDescentPolynomialRegression testForCurve = new GradientDescentPolynomialRegression(2);
+        testForCurve.addPoints(curve);
+        testForCurve.train();
 
     }
 }
